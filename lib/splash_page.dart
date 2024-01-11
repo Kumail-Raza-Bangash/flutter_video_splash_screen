527167680d75f020dddf0e1bf5b1e1659ca8ad7f
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-01-11 17:48:09 +0500

    first commit

---
+++ lib/splash_page.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_video_splash_screen/home_page.dart';
+import 'package:video_player/video_player.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+   late VideoPlayerController _controller;
+  bool _visible = false;
+
+   @override
+     void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    _controller = VideoPlayerController.asset("lib/assets/videos/V001.mp4");
+    _controller.initialize().then((_) {
+      _controller.setLooping(true);
+      Timer(const Duration(milliseconds: 100), () {
+        setState(() {
+          _controller.play();
+          _visible = true;
+        });
+      });
+    });
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const HomePage(param_homepage: 'Welcome Home')),
+          (e) => false);
+    });
+  }
+
+@override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    }
+
+  _getVideoBackground() {
+    return AnimatedOpacity(
+      opacity: _visible ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 1000),
+      child: VideoPlayer(_controller),
+    );
+  }
+
+  // ignore: unused_element
+  _getBackgroundColor() {
+    return Container(color: Colors.transparent //.withAlpha(120),
+        );
+  }
+
+  // ignore: unused_element
+  _getContent() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            _getVideoBackground(),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
